e07f6e37ef7f7f8357080f9cddb93c32c7cd490f
GuFvr <gustavo.vfavero@gmail.com>
2024-04-21 15:18:28 -0300

    init: First commit

---
+++ src/main/java/br/com/gufvr/firstspringbootproject/FirstSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.gufvr.firstspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootProjectApplication.class, args);
+	}
+
+}
