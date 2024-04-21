735f094862284faeb5f1a179819e98cb5a7ae420
GuFvr <gustavo.vfavero@gmail.com>
2024-04-21 17:32:39 -0300

    feat: Create FirstController with firstMethod
    Implemented a REST controller named FirstController with a GET method called firstMethod. The controller handles requests at the "/firstController/firstMethod" endpoint and returns the message "Primeira rota criada!" when accessed.

---
+++ src/main/java/br/com/gufvr/firstspringbootproject/FirstController.java
@@ -0,0 +1,19 @@
+package br.com.gufvr.firstspringbootproject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/firstController")
+public class FirstController {
+
+  @GetMapping("/firstMethod")
+  public String firstMethod() {
+      return "Primeira rota criada!";
+  }
+  
+
+}
--- src/main/java/br/com/gufvr/firstspringbootproject/FirstSpringbootProjectApplication.java
+++ src/main/java/br/com/gufvr/firstspringbootproject/FirstSpringbootProjectApplication.java
@@ -10,4 +10,4 @@ public class FirstSpringbootProjectApplication {
 		SpringApplication.run(FirstSpringbootProjectApplication.class, args);
 	}
 
-}
+} 	
